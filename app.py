f59b553fde6cc08b08171284d435c67ffcf47251
Elisee Mbaya <junimbaya@yahoo.fr>
2021-01-20 02:16:31 -0600

    Saves Filenames to saved.txt

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ root = tk.Tk()
 root.title("Auto Start Apps")
 apps = []
 
+# Load saved file
+
+if os.path.isfile('saved.txt'):
+    with open('saved.txt', 'r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
+
+
+def printFiles():
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="blue", fg="white",)
+        label.pack()
+
+
 # Add Apps Method
 
 
@@ -18,9 +33,7 @@ def addApp():
         initialdir="/", title=" Select An Application", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
     apps.append(filename)
     if len(apps) != 0:
-        for app in apps:
-            label = tk.Label(frame, text=app, bg="blue", fg="white",)
-            label.pack()
+        printFiles()
 
 
 def runApps():
@@ -43,5 +56,12 @@ addApp.pack()
 runApps = tk.Button(root, text="Run Apps", padx=10,
                     pady=5, fg="white", bg="green", command=runApps)
 runApps.pack()
+
+printFiles()
 # Run
 root.mainloop()
+
+# Saving data to file
+with open('saved.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
